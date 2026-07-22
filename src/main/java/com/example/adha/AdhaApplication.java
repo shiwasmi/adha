c2f6787b2578d30e9c1654fdc1cfacffcd1d9fd4
shiwasmi <sagarchattar@gmail.com>
2026-07-22 16:01:36 +0530

    Skelaton

---
+++ src/main/java/com/example/adha/AdhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.adha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhaApplication.class, args);
+	}
+
+}
